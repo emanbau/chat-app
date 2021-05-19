330c9ebabc62bf6cad14bd415497a5aee9e2dfa2
Eman Bau <emanbautista410@gmail.com>
2021-05-19 00:25:39 -0400

    ApolloProvider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,35 @@
 import React from 'react';
 import './App.scss';
+import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink, from } from '@apollo/client'
+import { onError } from '@apollo/client/link/error';
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    graphQLErrors.map(({ message }: any) => {
+      alert(`GraphQL error ${message}`);
+      return null;
+    });
+  }
+});
+
+const link = from([
+  errorLink, 
+  new HttpLink({ uri: "http://localhost:3001/graphql"})
+]);
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: link,
+})
+
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        
+      </div>
+    </ApolloProvider>
   );
 }
 
--- client/src/GraphQL/Queries/Mutations/User.ts
+++ client/src/GraphQL/Queries/Mutations/User.ts
@@ -1,1 +1,16 @@
 import { gql } from '@apollo/client';
+
+export const LOG_IN = gql`
+    query getUser(
+        $username: String!
+        $password: String!
+    ) {
+        getUser(
+            username: $username
+            password: $password
+        ) {
+            username
+            password
+        }
+    }
+`
