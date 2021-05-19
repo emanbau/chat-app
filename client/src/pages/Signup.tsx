eee4bfffcc8ce9edfa4cb3250b2e68bea04f1081
Eman Bau <emanbautista410@gmail.com>
2021-05-19 00:45:00 -0400

    States

--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useMutation } from '@apollo/client';
+import { SIGN_UP } from '../GraphQL/Mutations/User';
 
 interface Props {
     
 }
 
 const Signup: React.FC<Props> = ({}) => {
+    // Signup Field States
+    const [firstName, setFirstName] = useState<string>('');
+    const [lastName, setLastName] = useState<string>('');
+    const [username, setUsername] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+
+    // GraphQL Mutation
+    const [createUser, { error, loading, data }] = useMutation(SIGN_UP);
+    // Sign Up Button Handler
+    const signUp = async () => {
+
+    }
+
+    useEffect(() => {
+        if (error) console.error(error);
+        if (!loading) console.log(data);
+    }, [error, loading, data]);
+
     return (
         <div>
             
