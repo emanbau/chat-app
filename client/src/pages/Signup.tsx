a1d1211a0e067d63c1a4b262f7b74a061856a6c2
Eman Bau <emanbautista410@gmail.com>
2021-05-19 00:03:53 -0400

    Signup mutation

---
+++ client/src/pages/Signup.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface Props {
+    
+}
+
+const Signup: React.FC<Props> = ({}) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Signup;
