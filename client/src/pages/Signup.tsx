18087f5a403532907f53eec045e09881383f75f1
Eman Bau <emanbautista410@gmail.com>
2021-05-19 01:13:42 -0400

    Signup input fields & submit button

--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -47,7 +47,27 @@ const Signup: React.FC<Props> = ({}) => {
 
     return (
         <div>
-            
+            <input 
+                type="text"
+                placeholder="First Name"
+                onChange={(e) => setFirstName(e.target.value)}
+            />
+            <input 
+                type="text"
+                placeholder="Last Name"
+                onChange={(e) => setLastName(e.target.value)}
+            />
+            <input 
+                type="text"
+                placeholder="Username"
+                onChange={(e) => setUsername(e.target.value)}
+            />
+            <input 
+                type="text"
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <button onClick={signUp}>Launch</button>
         </div>
     )
 }
