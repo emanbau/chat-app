dfa5b3634d9ff89476d11a746ec7b63982f0bbd3
Eman Bau <emanbautista410@gmail.com>
2021-05-19 00:05:18 -0400

    Update sign up mutation

--- client/src/GraphQL/Mutations/User.ts
+++ client/src/GraphQL/Mutations/User.ts
@@ -1,1 +1,20 @@
 import { gql } from '@apollo/client';
+
+export const SIGN_UP = gql`
+    mutation createUser(
+        $firstName: String!
+        $lastName: String!
+        $username: String!
+        $password: String!
+    ) {
+        createUser(
+            firstName: $firstName
+            lastName: $lastName
+            username: $username
+            password; $password
+        ) {
+            successful
+            message
+        }
+    }
+`
