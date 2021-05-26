1d0355e97bb348ddf88e4d1c78a205f0c45d0b14
Eman Bau <emanbautista410@gmail.com>
2021-05-26 14:30:00 -0400

    Signup styles

--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -3,6 +3,7 @@ import { useMutation } from '@apollo/client';
 import { SIGN_UP } from '../GraphQL/Mutations/User';
 import { setUsername as reduxSetUsername, setPassword as reduxSetPassword } from '../Redux/account';
 import { useAppDispatch } from '../Redux/reduxhooks';
+import './Signup.scss';
 
 interface Props {
     noSignupHandle: () => void;
@@ -51,28 +52,34 @@ const Signup: React.FC<Props> = ({ noSignupHandle, loginHandle }) => {
     }, [error, loading, data]);
 
     return (
-        <div>
+        <div className="signup-container">
+            <h1 className="signup-heading">Signup</h1>
+            <p className="signup-caption">Start chatting in the browser today!</p>
             <input 
                 type="text"
                 placeholder="First Name"
                 onChange={(e) => setFirstName(e.target.value)}
+                className="first-name-input"
             />
             <input 
                 type="text"
                 placeholder="Last Name"
                 onChange={(e) => setLastName(e.target.value)}
+                className="last-name-input"
             />
             <input 
                 type="text"
                 placeholder="Username"
                 onChange={(e) => setUsername(e.target.value)}
+                className="username-input"
             />
             <input 
                 type="text"
                 placeholder="Password"
                 onChange={(e) => setPassword(e.target.value)}
+                className="password-input"
             />
-            <button onClick={signUp}>Launch</button>
+            <button className="signup-button" onClick={signUp}>Launch</button>
         </div>
     )
 }
